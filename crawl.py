9693461565faa499c52a8bc44be6e961ac783d0f
sainaa <tesatory@yahoo.com>
2013-02-08 13:25:08 +0900

    m

--- crawl.py
+++ crawl.py
@@ -1,28 +1,40 @@
 import re
 import urllib
 import Queue
+import threading
+
+def parse_url():
+    while True:
+        url = urls.get()
+        try:
+            src = urllib.urlopen(url).read()
+
+            links = re.findall(r"<a [^>]*href=\"([^\"]+)\"[^>]*>", src, re.M)
+
+            src = src.decode("utf-8")
+            cyr_words.extend(re.findall(r"(["+cyr_chars+"]+)", src, re.M))
+
+            if len(cyr_words) > 0:
+                for l in links:
+                    if l.startswith("http://"):
+                        urls.put(l)
+
+            print "words %d , links %d" % (len(cyr_words), urls.qsize())
+        except:
+            pass
+
+        urls.task_done()
 
 cyr_chars = open("mn_chars.txt").read().strip().decode("utf-8")
-
+cyr_words = list()
 urls = Queue.Queue()
 urls.put("http://www.gogo.mn")
 
-while urls.empty() == False:
-    url = urls.get()
-    try:
-        src = urllib.urlopen(url).read()
+workers = list()
+for i in range(10):
+    worker = threading.Thread(target = parse_url)
+    worker.setDaemon(True)
+    worker.start()
+    workers.append(worker)
 
-        links = re.findall(r"<a [^>]*href=\"([^\"]+)\"[^>]*>", src, re.M)
-
-        src = src.decode("utf-8")
-        cyr_words = re.findall(r"(["+cyr_chars+"]+)", src, re.M)
-        for w in cyr_words:
-            print w
-
-        if len(cyr_words) > 0:
-            for l in links:
-                if l.startswith("http://"):
-                    urls.put(l)
-
-    except:
-        pass
+urls.join()
