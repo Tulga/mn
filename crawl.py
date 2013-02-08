7c29bc5f5305ca013c207cdc0185cd6f2f904393
sainaa <tesatory@yahoo.com>
2013-02-08 13:57:15 +0900

    m

--- crawl.py
+++ crawl.py
@@ -6,6 +6,7 @@ import threading
 def parse_url():
     while True:
         url = urls.get(True)
+        print url
         try:
             src = urllib.urlopen(url).read()
 
@@ -16,12 +17,12 @@ def parse_url():
             for w in words:
                 cyr_words.put(w)
 
-            if len(cyr_words) > 0:
+            if len(words) > 0:
                 for l in links:
                     if l.startswith("http://"):
                         urls.put(l)
 
-            print "words %d , links %d" % (len(cyr_words), urls.qsize())
+            print "words %d , links %d" % (cyr_words.qsize(), urls.qsize())
         except:
             pass
 
@@ -30,7 +31,8 @@ def parse_url():
 def write_words():
     while True:
         w = cyr_words.get(True)
-        out.write(w + "\n")
+        out.write(w.encode("utf-8") + "\n")
+        cyr_words.task_done()
 
 char_file = open("mn_chars.txt")
 cyr_chars = char_file.read().strip().decode("utf-8")
@@ -38,6 +40,8 @@ char_file.close()
 cyr_words = Queue.Queue()
 urls = Queue.Queue()
 urls.put("http://www.gogo.mn")
+urls.put("http://www.news.mn")
+urls.put("http://www.olloo.mn")
 
 workers = list()
 for i in range(100):
@@ -52,4 +56,5 @@ writer.setDaemon(True)
 writer.start()
 
 urls.join()
+cyr_words.join()
 out.close()
