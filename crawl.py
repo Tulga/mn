2970598b48c17f4022caf50041e4717f4f7b5095
sainaa <tesatory@yahoo.com>
2013-02-08 12:43:04 +0900

    m

--- crawl.py
+++ crawl.py
@@ -1,16 +1,28 @@
 import re
 import urllib
+import Queue
 
 cyr_chars = open("mn_chars.txt").read().strip().decode("utf-8")
 
-url = "http://www.gogo.mn"
-src = urllib.urlopen(url).read()
+urls = Queue.Queue()
+urls.put("http://www.gogo.mn")
 
-links = re.findall(r"<a [^>]*href=\"([^\"]+)\"[^>]*>", src, re.M)
-for l in links:
-    print l
+while urls.empty() == False:
+    url = urls.get()
+    try:
+        src = urllib.urlopen(url).read()
 
-src = src.decode("utf-8")
-cyr_words = re.findall(r"(["+cyr_chars+"]+)", src, re.M)
-for w in cyr_words:
-    print w
+        links = re.findall(r"<a [^>]*href=\"([^\"]+)\"[^>]*>", src, re.M)
+
+        src = src.decode("utf-8")
+        cyr_words = re.findall(r"(["+cyr_chars+"]+)", src, re.M)
+        for w in cyr_words:
+            print w
+
+        if len(cyr_words) > 0:
+            for l in links:
+                if l.startswith("http://"):
+                    urls.put(l)
+
+    except:
+        pass
